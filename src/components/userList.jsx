b9dec440ef1a245b034eed5bf5049da11dd6d368
sousalmdev <leandromedeiros690@gmail.com>
2024-06-05 18:50:13 -0300

    Fix: Style

--- src/components/userList.jsx
+++ src/components/userList.jsx
@@ -5,14 +5,14 @@ const UserList = () => {
   const users = useStore((state) => state.users);
 
   return (
-    <div id="tabela" className="flex items-center flex-col pt-20">
+    <div id="tabela" className="flex items-center flex-col pt-20 ">
       <div className="bg-user bg-cover bg-center w-full">
         <h1 className="w-full bg-black/20 backdrop-blur-sm font-semibold   py-10 md:py-20 md:text-6xl text-3xl text-center">
           Administração de Usuários
         </h1>
       </div>
-      <div className="flex mt-10 flex-col items-center w-full justify-center">
-        <table className="border-collapse text-center w-11/12">
+      <div className="flex mt-10 flex-col items-center w-full justify-center px-2">
+        <table className="border-collapse text-center w-10/12">
           <thead>
             <tr>
               <th className="border bg-havGreen text-black p-2">Nome</th>
